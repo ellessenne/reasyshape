461b3c69d38c0a95cdee62cd145ce45bcaa6c1a2
Alessandro Gasparini <ellessenne@gmail.com>
2020-03-25 17:40:06 +0100

    First basic implementation

--- inst/application/server.R
+++ inst/application/server.R
@@ -1,23 +1,125 @@
-function(input, output) {
+function(input, output, session) {
 
-  output$main_plot <- renderPlot({
+  ### Reading data
+  data <- shiny::reactive({
+    shiny::req(input$loadData)
+    inFile <- input$loadData
+    ptn <- "\\.[[:alnum:]]{1,8}$"
+    suf <- tolower(regmatches(inFile$name, regexpr(ptn, inFile$name)))
+    df <- switch(
+      suf,
+      ".csv" = readr::read_csv(inFile$datapath),
+      ".dta" = haven::read_dta(inFile$datapath),
+      ".sav" = haven::read_sav(inFile$datapath),
+      ".sas7bdat" = haven::read_sas(inFile$datapath),
+      ".rds" = readRDS(inFile$datapath)
+    )
+    attr(df, "spec") <- NULL
+    df <- labelled::remove_labels(df, user_na_to_na = TRUE)
+    df <- tibble::as_tibble(df)
+    return(df)
+  })
 
-    hist(faithful$eruptions,
-         probability = TRUE,
-         breaks = as.numeric(input$n_breaks),
-         xlab = "Duration (minutes)",
-         main = "Geyser eruption duration")
+  ### Put columns in Values and Names select inputs
+  shiny::observe({
+    shiny::req(data())
+    shiny::updateSelectInput(
+      session,
+      inputId = "valuesInput",
+      choices = names(data())
+    )
+    shiny::updateSelectInput(
+      session,
+      inputId = "namesInput",
+      choices = names(data())
+    )
+    shiny::updateSelectInput(
+      session,
+      inputId = "colsInput",
+      choices = names(data())
+    )
+  })
 
-    if (input$individual_obs) {
-      rug(faithful$eruptions)
+  ### Reshape
+  reshape <- shiny::reactive({
+    shiny::req(data())
+    msg <- "Please define inputs appropriately."
+    if (input$wideOrLong == "wide") {
+      shiny::validate(
+        shiny::need(!is.null(input$valuesInput) & !is.null(input$namesInput), message = msg)
+      )
+      tidyr::pivot_wider(
+        data = data(),
+        names_from = tidyr::one_of(input$namesInput),
+        values_from = tidyr::one_of(input$valuesInput)
+      )
+    } else {
+      shiny::validate(
+        shiny::need(!is.null(input$colsInput) & input$namesTo != "" & input$valuesTo != "", message = msg)
+      )
+      tidyr::pivot_longer(
+        data = data(),
+        cols = tidyr::one_of(input$colsInput),
+        names_to = input$namesTo,
+        values_to = input$valuesTo
+      )
     }
+  })
 
-    if (input$density) {
-      dens <- density(faithful$eruptions,
-                      adjust = input$bw_adjust)
-      lines(dens, col = "blue")
+  ### Make download buttons only when data() is loaded
+  shiny::observe({
+    shiny::req(reshape())
+    output$downloadButtonUI <- shiny::renderUI(
+      shiny::downloadButton(
+        outputId = "downloadButton",
+        label = "Download!",
+        icon = shiny::icon("download")
+      )
+    )
+  })
+
+  ### Downloader
+  output$downloadButton <- shiny::downloadHandler(
+    filename = function() {
+      extension <- switch(
+        input$fileFormat,
+        "csv" = ".csv",
+        "tsv" = ".tsv",
+        "r" = ".rds",
+        "stata" = ".dta",
+        "spss" = ".sav",
+        "sas" = ".sas7bdat"
+      )
+      paste0(input$fileName, extension)
+    },
+    content = function(file) {
+
+      ### Here goes the code to save data
+      out <- reshape()
+
+      if (input$fileFormat == "csv") {
+        readr::write_csv(x = out, path = file)
+      } else if (input$fileFormat == "tsv") {
+        readr::write_tsv(x = out, path = file)
+      } else if (input$fileFormat == "r") {
+        saveRDS(object = out, file = file)
+      } else if (input$fileFormat == "stata") {
+        haven::write_dta(data = out, path = file)
+      } else if (input$fileFormat == "spss") {
+        haven::write_sav(data = out, path = file)
+      } else if (input$fileFormat == "sas") {
+        haven::write_sas(data = out, path = file)
+      }
     }
+  )
 
+  output$previewInput <- DT::renderDT({
+    shiny::req(data())
+    data()
+  })
+
+  output$previewReshape <- DT::renderDT({
+    shiny::req(reshape())
+    reshape()
   })
 }
-
--- inst/application/ui.R
+++ inst/application/ui.R
@@ -1,28 +1,98 @@
-
-
-bootstrapPage(
-
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
-
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
-
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
-
-  plotOutput(outputId = "main_plot", height = "300px"),
-
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-                   sliderInput(inputId = "bw_adjust",
-                               label = "Bandwidth adjustment:",
-                               min = 0.2, max = 2, value = 1, step = 0.2)
+shiny::navbarPage(
+  title = "reasyshape",
+  inverse = TRUE,
+  collapsible = TRUE,
+  fluid = TRUE,
+  theme = shinythemes::shinytheme("flatly"),
+  shiny::tabPanel(
+    title = "Import Data",
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::fileInput(
+          inputId = "loadData",
+          label = "Input file"
+        )
+      ),
+      shiny::mainPanel(
+        DT::DTOutput(outputId = "previewInput")
+      )
+    )
+  ),
+  shiny::tabPanel(
+    title = "Reshape Data",
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::radioButtons(
+          inputId = "wideOrLong",
+          label = "Direction:",
+          choices = c(
+            "Long to Wide" = "wide",
+            "Wide to Long" = "long"
+          )
+        ),
+        shiny::conditionalPanel(
+          condition = "input.wideOrLong == 'wide'",
+          shiny::selectInput(
+            inputId = "valuesInput",
+            label = "Values from:",
+            choices = "",
+            multiple = TRUE
+          ),
+          shiny::selectInput(
+            inputId = "namesInput",
+            label = "Names from:",
+            choices = "",
+            multiple = TRUE
+          )
+        ),
+        shiny::conditionalPanel(
+          condition = "input.wideOrLong == 'long'",
+          shiny::selectInput(
+            inputId = "colsInput",
+            label = "Columns:",
+            choices = "",
+            multiple = TRUE
+          ),
+          shiny::textInput(
+            inputId = "namesTo",
+            label = "Column with column names:",
+            value = "name"
+          ),
+          shiny::textInput(
+            inputId = "valuesTo",
+            label = "Column with column values:",
+            value = "value"
+          )
+        )
+      ),
+      shiny::mainPanel(
+        DT::DTOutput(outputId = "previewReshape")
+      )
+    )
+  ),
+  shiny::tabPanel(
+    title = "Export Data",
+    shiny::sidebarPanel(
+      shiny::textInput(
+        inputId = "fileName",
+        label = "File name:",
+        value = "new-file"
+      ),
+      shiny::selectInput(
+        inputId = "fileFormat",
+        label = "Format of the file to export:",
+        choices = c(
+          "Comma-separated" = "csv",
+          "Tab-separated" = "tsv",
+          "R" = "r",
+          "Stata" = "stata",
+          "SPSS" = "spss",
+          "SAS" = "sas"
+        ),
+        selected = "csv"
+      ),
+      shiny::uiOutput(outputId = "downloadButtonUI")
+    ),
+    shiny::mainPanel()
   )
-
 )
-
